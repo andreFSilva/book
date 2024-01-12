f809ba1d8bab2347682f39ccee8d29866552e0ee
andreFSilva <andrealekhine@gmail.com>
2024-01-12 20:17:39 -0300

    Terminando o basico e indo para a parte dois.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
+app_name = "blog"
+
 
 urlpatterns = [
+    # path("", views.post_list, name="post_list"), # Url fbv
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('',views.PostListView.as_view(), name='post_list'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # três postagens em cada página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger: # se a pagina não for um inteiro, exibe a primeria página
+        posts = paginator.page(1)
+    except EmptyPage: # se a página for maior que o total de páginas, exibe a última página
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request,'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
