cbf1c994997622230bdea731ba1c7ac7d90e505e
andreFSilva <andrealekhine@gmail.com>
2024-01-15 17:30:52 -0300

    atualizando.

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,58 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # três postagens em cada página
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
-    except PageNotAnInteger: # se a pagina não for um inteiro, exibe a primeria página
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
-    except EmptyPage: # se a página for maior que o total de páginas, exibe a última página
+    except EmptyPage:
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request,
+                 'blog/post/list.html',
+                 {'page': page,
+                  'posts': posts,
+                  'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request,'blog/post/detail.html', {'post': post})
+    # Lista dos comentários ativos para esta postagem
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # Um comentário foi postado
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Cria o objeto Comment, mas não o salva inda no banco de dados
+            new_comment = comment_form.save(commit=False)
+            # Atribui a postagem atual ao comentário.
+            new_comment.post = post
+            # Salva o comentário no banco de dados.
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post/detail.html', {'post': post,
+                                                    'comments':comments,
+                                                    'new_comment': new_comment,
+                                                    'comment_form': comment_form})
 
 class PostListView(ListView):
     queryset = Post.published.all()
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # app terceiros
     'django_extensions',
+    'taggit',
     # app local
     'blog.apps.BlogConfig',
     #'django_app',
